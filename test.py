1a0da162e936bc815d7ac1d3682bc98555329383
promiseaborishade05-web <promiseaborishade05@gmail.com>
2025-12-02 23:51:40 +0100

    initial commit

---
+++ test.py
@@ -0,0 +1,35 @@
+#velocity = distance / time
+distance = float(input('Enter distance: '))
+time = float(input('Enter time taken to cover that distance: '))
+print('velocity = ', distance / time)
+
+#Data
+#Alice->19->50kg->165.7cm
+#variable
+name = input('Enter your name')  #string
+age = int(input('Enter age'))  #integer
+weight = 70
+height = float(input('Enter height'))  #float '170.4' -> 170.4
+
+print(name, ' is ', 2025 - age)
+
+#calculate force = mass = acceleration
+mass = 50
+acceleration = 30
+
+force = mass * acceleration
+print(force)
+
+
+if age >= 20 or height >= 1.7:
+      print('you are eligible')
+else:
+    print('you are not eligible')
+
+
+
+
+
+
+
+    
